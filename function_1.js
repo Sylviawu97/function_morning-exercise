2fa41426e554ba3bd75959af099ad72ac99108d0
Meixuan Wu <mwu@LABA.PSWAS.LOCAL>
2023-04-04 10:00:00 -0700

    morning exercise

---
+++ function_1.js
@@ -0,0 +1,35 @@
+//1
+ 
+const countFromOneToTen =(num)=>{
+    if(num=0;num<=10;num++){
+        console.log(num);
+    }
+ }
+ countFromOneToTen(10)
+ 
+ //2
+ const sayHello =(name) =>{
+    console.log("Hello"+name)
+ }
+ sayHello('Sylvia')
+
+ //3
+ const output=(numberFromArray)=>{
+    let array=[1,2,3,4,5,6]
+    if(numberFromArray<array.length){
+        console.log(false)
+    }
+ }
+ output(3)
+
+ // 4
+ const age =(number)=>{
+    console.log(number)
+ }
+ age(23)
+
+ //5
+ const sayGoodbye =(words)=>{
+    console.log=('Goodebye'+words)
+ }
+ sayGoodbye()
